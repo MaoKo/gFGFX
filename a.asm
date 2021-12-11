db8d7c529b927b86e04eeecddb8a2b17a29ae768
Alexandre <achiu-au@student.42.fr>
2021-12-11 01:42:20 +0100

    arrange some cfg procedure

--- a.asm
+++ a.asm
@@ -17,7 +17,7 @@ include "parser.inc"
 ;decl_and_compile? B, "B"
 
 ;_regex?._connect? A, B
-;_regex?._generate_lookup? test, A
+;_regex?._generate_lookup? test, A, _constant?._false?
 
 ;_automata?._declare? a1
 ;_automata?._new_state? a1, _constant?._true?, _constant?._false?
@@ -42,12 +42,27 @@ include "parser.inc"
 ;_bitset?._cache_retreive? _ret, _suc, _test_1, {1H}
 ;_bitset?._cache_clear? *
 
-_regex?._declare? _register, _constant?._false?
-_regex?._compile? _register, "(x|y|z)mm([0-9]|(1[0-9])|(2[0-9])|30|31)"
+;_regex?._declare? _register, _constant?._false?
+;_regex?._compile? _register, "(x|y|z)mm([0-9]|(1[0-9])|(2[0-9])|30|31)"
 ;repeat 0FFFFH
-_regex?._match? _check, _register, "xmm1ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd"
-db _check
-_regex?._match? _check, _register, "xmm1"
-db _check
+;_regex?._match? _check, _register, "xmm1ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd"
 ;end repeat
+
+;A? = 3
+;B? = 5
+;D? = 6
+
+;_parser?._declare? _ABC
+;_parser?._nonterminal? _ABC, _constant?._true?, <S>
+;_parser?._nonterminal? _ABC, _constant?._false?, <L>
+;_parser?._nonterminal? _ABC, _constant?._false?, <M>
+;_parser?._productions? _ABC, <L> -> (A), (B), <M>, (D)
+;_parser?._productions? _ABC, <E> -> (A), (B), <A>, (E)
+;_parser?._productions? _ABC, <E> -> (A), (B), <A>
+;_parser?._productions? _ABC, <E> -> (A), <A>
+
+
+;<D> -> <A>
+;<A> -> <B>, (T), <B>
+;<B> -> (C)|
 
